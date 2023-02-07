36b43519db5849ad0ccfc77293adcb7c91cbb5d8
luizfernando <luiz-fsilva37@hotmail.com>
2023-02-06 22:54:19 -0300

    feat: add validations in user module

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
 export default class UserDTO {
+  @IsNotEmpty({
+    message: 'Username cannot be empty',
+  })
   name: string;
+
+  @IsEmail(undefined, {
+    message: 'Username cannot be empty',
+  })
   email: string;
+
+  @MinLength(6, {
+    message: 'Password less than 6 characters',
+  })
   password: string;
 }
